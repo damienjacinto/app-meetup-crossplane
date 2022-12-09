0899a884b503bb22283166b2d480c480c408cffb
damien jacinto <damien.jacinto@gmail.com>
2022-12-09 23:43:56 +0100

    add linter

--- app/date_calendar.py
+++ app/date_calendar.py
@@ -1,17 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
 from database import db
 import uuid
+
 
 def generate_uuid():
     return str(uuid.uuid4())
 
+
 class DateCalender(db.Model):
     id = db.Column(db.String, unique=True, nullable=False, primary_key=True, default=generate_uuid)
     date = db.Column(db.Integer, unique=True, nullable=False)
-
-    def __init__(self, id, date):
-        self.id = id
-        self.date = date
 
     def __init__(self, date):
         self.id = None
--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
 import json
 from date_calendar import DateCalender
 from database import db
@@ -12,13 +11,15 @@ app = Flask(__name__,
 database_enable = bool(os.environ.get('DATABASE_ENABLE', ""))
 
 if database_enable:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
+    database_url = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     db.init_app(app)
     with app.app_context():
         db.create_all()
 
 f = open('./app/data.json')
 data = json.load(f)
+
 
 @app.route("/")
 def index():
@@ -32,15 +33,16 @@ def index():
                     message["opened"] = "greyImg"
     return render_template('index.html', messages=data['messages'], title=title)
 
+
 @app.route('/update', methods=['POST'])
 def update():
-    form = request.form
     if database_enable:
         date = request.form['date']
-        dc = DateCalender(date = date)
+        dc = DateCalender(date=date)
         db.session.add(dc)
         db.session.commit()
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
