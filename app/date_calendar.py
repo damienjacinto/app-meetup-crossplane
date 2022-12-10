d330b920b263d85afe2d37df4a7ada05962b1427
damien jacinto <damien.jacinto@gmail.com>
2022-12-11 00:49:48 +0100

    create preview

---
+++ app/date_calendar.py
@@ -0,0 +1,18 @@
+from database import db
+import uuid
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+class DateCalender(db.Model):
+    id = db.Column(db.String, unique=True, nullable=False, primary_key=True, default=generate_uuid)
+    date = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __init__(self, date):
+        self.id = None
+        self.date = date
+
+    def __repr__(self):
+        return f"<id={self.id}, date={self.date}>"
